5fd8607e15238ec7a7c8f5b21cd1450c27e22a73
Sunil Kumar <sunil@sun1l.com>
2017-03-09 12:52:39 +0530

    fix: Fixed escaping of non React component files, and some refactor

--- src/index.js
+++ src/index.js
@@ -1,50 +1,64 @@
-import {parse, defaultHandlers} from 'react-docgen';
+import {parse, defaultHandlers as handlers} from 'react-docgen';
 import componentNameHandler from './handlers/componentNameHandler';
 
-
-// Fetch reactDoc handlers and merge
-let handlers = defaultHandlers;
+// Merge custom handlers with reactDoc defaultHandlers
 handlers.unshift(componentNameHandler);
 
 let json = {};
 
-function ReactExtractComponentMeta(config = {
-  'entry': /components/g,
-  'output':{
-    'filename': 'component-meta.json'
+function ReactExtractComponentMeta(config = {}) {
+  this.config = {};
+  let configDefaults = {
+    'filter': /component/gi,
+    'output': {
+      'filename': 'components-meta.json'
+    }
   }
-}) {
-  this.config = config;
+  
+  Object.assign(this.config, configDefaults, config);
 }
 
-ReactExtractComponentMeta.prototype.apply = function(compiler) {
+ReactExtractComponentMeta.prototype.apply = function (compiler) {
 
-  compiler.plugin('emit', function(compilation, callback) {
+  // Get compilation instance
+  compiler.plugin('emit', function (compilation, callback) {
 
-    // Explore each chunk (build output):
-    compilation.chunks.forEach(function(chunk) {
+    // Explore each chunk in the compilation
+    compilation.chunks.forEach(function (chunk) {
 
-      // Explore each module within the chunk (built inputs):
-      chunk.modules.forEach(function(module) {
-        
-        let resourcePath = module.resource;
+      // Explore each module within the chunk
+      chunk.modules.forEach(function (module) {
 
-        // Filter react components
-        if(Array.isArray(resourcePath.match(this.config.entry))){
-          let source = module._source._value; 
-          let componentInfo = parse(source, null, handlers);
-          
-          let getComponentName = componentInfo.name;
-          json[getComponentName] = componentInfo;
+        let resourcePath = module.rawRequest;
+
+        // Filter through the valid files as specified in config
+        if (Array.isArray(resourcePath.match(this.config.filter))) {
+          let source = module._source._value;
+          let componentInfo = {};
+
+          // Parse the source with reactDoc.parse
+          try{
+            componentInfo = parse(source, null, handlers);
+          }
+          catch(e){
+            console.log(resourcePath + " is not a React Component" + "\n");
+          }
+
+          if(componentInfo.name){
+            let getComponentName = componentInfo.name;
+            json[getComponentName] = componentInfo;
+          }
         }
       }, this);
     }, this);
 
-  compilation.assets[this.config.output.filename] = {
-      source: function() {
-        return JSON.stringify(json);
+    // Add the JSON file in assets
+    json = JSON.stringify(json);
+    compilation.assets[this.config.output.filename] = {
+      source: function () {
+        return json;
       },
-      size: function() {
+      size: function () {
         return json.length;
       }
     };
